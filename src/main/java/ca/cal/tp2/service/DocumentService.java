1df56db6f6b21b1f01a2abae93911151b926869f
dray777 <iliesseaisset@gmail.com>
2025-03-11 06:03:56 -0400

    Document Service fait

--- src/main/java/ca/cal/tp2/service/DocumentService.java
+++ src/main/java/ca/cal/tp2/service/DocumentService.java
@@ -1,4 +1,51 @@
 package ca.cal.tp2.service;
 
+import ca.cal.tp2.persistance.DocumentDAO;
+import ca.cal.tp2.modele.Document;
+import ca.cal.tp2.modele.Livre;
+
+import java.util.List;
+
 public class DocumentService {
+
+    private final DocumentDAO documentDAO;
+
+    public DocumentService(DocumentDAO documentDAO) {
+        this.documentDAO = documentDAO;
+    }
+
+
+    public void createDocument(Document document) {
+        documentDAO.save(document);
+    }
+
+
+    public void updateDocument(Document document) {
+        documentDAO.update(document);
+    }
+
+
+    public void deleteDocument(Document document) {
+        documentDAO.delete(document);
+    }
+
+
+    public Document findDocumentById(int id) {
+        return documentDAO.findById(id);
+    }
+
+
+    public List<Document> listAllDocuments() {
+        return documentDAO.findAll();
+    }
+
+
+    public List<Document> findDocumentsByTitre(String motCle) {
+        return documentDAO.findByTitre(motCle);
+    }
+
+
+    public List<Livre> findLivresByAuteur(String motCle) {
+        return documentDAO.findLivreByAuteur(motCle);
+    }
 }
