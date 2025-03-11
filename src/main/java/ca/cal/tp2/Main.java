eb98852d73d89753e54d644c55a35999040c88be
dray777 <iliesseaisset@gmail.com>
2025-03-11 03:41:53 -0400

    Test de hibernate et correction de la structure

--- src/main/java/ca/cal/tp2/Main.java
+++ src/main/java/ca/cal/tp2/Main.java
@@ -1,7 +1,13 @@
 package ca.cal.tp2;
 
+import ca.cal.tp2.modele.CD;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
 public class Main {
     public static void main(String[] args) {
-        // Votre script qui utilise votre API ici
+        }
     }
-}
+
