a91cbe7d762de90ce76c15a71349df278cc06f25
dray777 <iliesseaisset@gmail.com>
2025-03-11 06:05:26 -0400

    Emprunt Service fait

--- src/main/java/ca/cal/tp2/service/EmpruntService.java
+++ src/main/java/ca/cal/tp2/service/EmpruntService.java
@@ -1,4 +1,46 @@
 package ca.cal.tp2.service;
 
+import ca.cal.tp2.modele.Emprunt;
+import ca.cal.tp2.modele.Emprunteur;
+import ca.cal.tp2.persistance.EmpruntDAO;
+
+import java.util.List;
+
 public class EmpruntService {
+
+    private final EmpruntDAO empruntDAO;
+
+    public EmpruntService(EmpruntDAO empruntDAO) {
+        this.empruntDAO = empruntDAO;
+    }
+
+
+    public void createEmprunt(Emprunt emprunt) {
+        empruntDAO.save(emprunt);
+    }
+
+
+    public Emprunt findEmpruntById(int id) {
+        return empruntDAO.findById(id);
+    }
+
+
+    public List<Emprunt> findAllEmprunts() {
+        return empruntDAO.findAll();
+    }
+
+
+    public List<Emprunt> findEmpruntsByEmprunteur(Emprunteur emprunteur) {
+        return empruntDAO.findByEmprunteur(emprunteur);
+    }
+
+
+    public void updateEmprunt(Emprunt emprunt) {
+        empruntDAO.update(emprunt);
+    }
+
+
+    public void deleteEmprunt(Emprunt emprunt) {
+        empruntDAO.delete(emprunt);
+    }
 }
